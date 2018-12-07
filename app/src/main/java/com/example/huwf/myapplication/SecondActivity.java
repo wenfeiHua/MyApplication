4714b87991dbfb164dc34e45f7dec78af5e6521a
wenfeiHua <2387746844@qq.com>
2018-12-07 09:57:12 +0800

    app

---
+++ app/src/main/java/com/example/huwf/myapplication/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.huwf.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+//        ImageView imageView=(ImageView) findViewById(R.id.svg_view);
+//        imageView.setImageDrawable(getDrawable(R.drawable.ic_img_1));
+    }
+}
